fd04e5d28d9b61bca2490af3d5a5e4b568c9b031
Gchan <rubengonzaleznavia@gmail.com>
2020-04-07 17:50:52 +0900

    CRUD Done (but update) Tested and everything goes well

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items')
+
 const app = express();
 
 //Bodyparser Middleware
@@ -18,6 +20,9 @@ mongoose
     })
     .then(() => console.log("MongoDB connected..."))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
