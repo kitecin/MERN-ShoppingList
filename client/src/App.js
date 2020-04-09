8ad57bad9da02df47b295510fc894f28e34f061b
Gchan <rubengonzaleznavia@gmail.com>
2020-04-09 18:45:53 +0900

    feat(Client): Added ShoppingList Component & Transitions
    Added ShoppingList Component & Transitions to the React Client side,
    next step is implement Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList'
 
 import 'bootstrap/dist/css/bootstrap.min.css'; //Import bootstrap to change the appearance
 import './App.css';
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList/>
     </div>
   );
 }
